377452c0c35d7e8d371e3b59b26dd93acd500168
Voloshin_Sergey <4voloshin@gmail.com>
2020-11-28 22:26:54 +0300

    feat: add burger menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
-let swiper = new Swiper('.slider-container', {
+const swiper1 = document.querySelector('.slider-container');
+const burger = document.querySelector('.burger');
+const close = document.querySelector('.menu__close');
+const menu = document.querySelector('.menu');
+
+let swiper = new Swiper(swiper1, {
   centeredSlides: true,
   slidesPerView: 'auto',
   loop: true,
   spaceBetween: 105,
 });
+
+burger.addEventListener('click', () => {
+  menu.classList.add('menu--visible');
+});
+
+close.addEventListener('click', () => {
+  menu.classList.remove('menu--visible');
+});
+
