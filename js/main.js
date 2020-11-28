614ca553c18be5c7dfd29f47ae373b3efbbdcd65
Voloshin_Sergey <4voloshin@gmail.com>
2020-11-29 00:38:06 +0300

    feat: add first slider

--- js/main.js
+++ js/main.js
@@ -1,13 +1,41 @@
 const swiper1 = document.querySelector('.slider-container');
+const swiper2 = document.querySelector('.swiper-container');
 const burger = document.querySelector('.burger');
 const close = document.querySelector('.menu__close');
 const menu = document.querySelector('.menu');
+const playButtonFirst = document.querySelectorAll('.main-slider__play');
 
-let swiper = new Swiper(swiper1, {
+let swiperSlider1 = new Swiper(swiper1, {
   centeredSlides: true,
   slidesPerView: 'auto',
   loop: true,
   spaceBetween: 105,
+});
+
+let swiperSlider2 = new Swiper(swiper2, {
+  centeredSlides: true,
+  slidesPerView: 1,
+  loop: true,
+  spaceBetween: 10,
+  fadeEffect: {
+    crossFade: true
+  },
+  effect: 'fade',
+  navigation: {
+    nextEl: '.btn-right',
+    prevEl: '.btn-left',
+  },
+});
+
+swiperSlider2.on('transitionEnd', function () {
+  let videos = document.querySelectorAll('.first__slider video');
+  videos.forEach((el) => {
+    el.pause();
+    el.currentTime = 0;
+  });
+  playButtonFirst.forEach((el) => {
+    el.style.display = 'block';
+  })
 });
 
 burger.addEventListener('click', () => {
@@ -18,3 +46,13 @@ close.addEventListener('click', () => {
   menu.classList.remove('menu--visible');
 });
 
+playButtonFirst.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    let video = e.currentTarget.closest('.main-slider__media').querySelector('video');
+    video.play();
+    e.currentTarget.style.display = 'none';
+    setTimeout(() => {
+      video.volume = 1;
+    }, 1000);
+  });
+});
